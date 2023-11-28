50caefe67f8d792af4396a2b60409379e343db38
Aran Gilman <f.zoniventris@gmail.com>
2023-11-27 19:19:13 -0800

    Create MeshData class

---
+++ ModelLoading/MeshData.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/glm.hpp>
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 uv;
+};
+
+struct MeshData
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+};
+
