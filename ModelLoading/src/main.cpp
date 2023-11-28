28c4ffb335ef8d078fe5230f000028d72bd6287b
Aran Gilman <f.zoniventris@gmail.com>
2023-11-28 10:39:25 -0800

    First pass at adding shader code

--- ModelLoading/src/main.cpp
+++ ModelLoading/src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 
+#include <GL/glew.h>
+
 #include "Window.h"
 
+const char* vertexShaderSource = R"s(
+#version 460 core
+layout (location = 0) in vec3 inPos;
+
+void main()
+{
+    gl_Position = vec4(inPos, 1.0);
+})s";
+
+const char* fragmentShaderSource = R"s(
+#version 460 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = (0.5f, 0.5f, 0.5f, 1.0f);
+})s";
+
 int main(int argc, char* argv[]) {
-	Window(800, 600, "Model Loading Test").Display();
+	Window window(800, 600, "Model Loading Test");
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+
+	window.Display();
 	return 0;
 }
