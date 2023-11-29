a414c1bb391e045f80a86c933bf7f5f94002f481
Aran Gilman <f.zoniventris@gmail.com>
2023-11-28 22:18:02 -0800

    Add remaining cube faces

--- ModelLoading/src/MeshData.cpp
+++ ModelLoading/src/MeshData.cpp
@@ -47,10 +47,62 @@ MeshData CreateCube()
         {h},
         {g},
         {e},
+
+        // Right
+        {b},
+        {d},
+        {h},
+        {f},
+
+        // Back
+        {a},
+        {c},
+        {d},
+        {b},
+
+        // Left
+        {e},
+        {g},
+        {c},
+        {a},
+
+        // Top
+        {b},
+        {f},
+        {e},
+        {a},
+
+        // Bottom
+        {h},
+        {d},
+        {c},
+        {g},
     };
-    mesh.indices = {   
+
+    mesh.indices = {
+        // Front
         0, 1, 3,
         1, 2, 3,
+
+        // Right
+        4, 5, 7,
+        5, 6, 7,
+
+        // Back
+        8, 9, 11,
+        9, 10, 11,
+
+        // Left
+        12, 13, 15,
+        13, 14, 15,
+
+        // Top
+        16, 17, 19,
+        17, 18, 19,
+
+        // Bottom
+        20, 21, 23,
+        21, 22, 23,
     };
     return mesh;
 }
