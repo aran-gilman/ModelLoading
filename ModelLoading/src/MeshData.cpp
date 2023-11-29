e13247611924774168cb001443b049fcaec0c852
Aran Gilman <f.zoniventris@gmail.com>
2023-11-28 22:12:30 -0800

    Add ASCII art, vars to help construct cube

--- ModelLoading/src/MeshData.cpp
+++ ModelLoading/src/MeshData.cpp
@@ -16,14 +16,37 @@ MeshData CreateQuad()
     return mesh;
 }
 
+/*
+*
+*  a - - - - - b
+*   | \     | \
+*   | e - - - - - f
+*   |   |   |   |
+* c - - | - - d |
+*     \ |     \ |
+*     g - - - - - h
+*
+*   ASCII art generated with https://1j01.github.io/ascii-hypercube/
+*/
 MeshData CreateCube()
 {
+    glm::vec3 a{ -0.5f,  0.5f, -0.5f };
+    glm::vec3 b{  0.5f,  0.5f, -0.5f };
+    glm::vec3 c{ -0.5f, -0.5f, -0.5f };
+    glm::vec3 d{  0.5f, -0.5f, -0.5f };
+
+    glm::vec3 e{ -0.5f,  0.5f,  0.5f };
+    glm::vec3 f{  0.5f,  0.5f,  0.5f };
+    glm::vec3 g{ -0.5f, -0.5f,  0.5f };
+    glm::vec3 h{  0.5f, -0.5f,  0.5f };
+
     MeshData mesh;
     mesh.vertices = {
-        {{  0.5f,  0.5f, 0.5f }},
-        {{  0.5f, -0.5f, 0.5f }},
-        {{ -0.5f, -0.5f, 0.5f }},
-        {{ -0.5f,  0.5f, 0.5f }},
+        // Front
+        {f},
+        {h},
+        {g},
+        {e},
     };
     mesh.indices = {   
         0, 1, 3,
