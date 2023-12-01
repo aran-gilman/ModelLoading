4798856ddbef9dcf772576f9aab060940f3938ae
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 12:56:19 -0800

    Move CreateQuad() definition back to MeshData.cpp

--- ModelLoading/src/MeshData.cpp
+++ ModelLoading/src/MeshData.cpp
@@ -2,6 +2,22 @@
 
 #include "VectorConstants.h"
 
+MeshData CreateQuad()
+{
+    MeshData mesh;
+    mesh.vertices = {
+        {{  0.5f,  0.5f, 0.0f }},  // top right
+        {{  0.5f, -0.5f, 0.0f }},  // bottom right
+        {{ -0.5f, -0.5f, 0.0f }},  // bottom left
+        {{ -0.5f,  0.5f, 0.0f }},  // top left 
+    };
+    mesh.indices = {
+        0, 1, 3,
+        1, 2, 3,
+    };
+    return mesh;
+}
+
 /*
 *
 *  a - - - - - b
--- ModelLoading/src/Model.cpp
+++ ModelLoading/src/Model.cpp
@@ -49,22 +49,6 @@ namespace
     }
 }
 
-MeshData CreateQuad()
-{
-    MeshData mesh;
-    mesh.vertices = {
-        {{  0.5f,  0.5f, 0.0f }},  // top right
-        {{  0.5f, -0.5f, 0.0f }},  // bottom right
-        {{ -0.5f, -0.5f, 0.0f }},  // bottom left
-        {{ -0.5f,  0.5f, 0.0f }},  // top left 
-    };
-    mesh.indices = {
-        0, 1, 3,
-        1, 2, 3,
-    };
-    return mesh;
-}
-
 Model LoadModel(const std::string& path)
 {
     Assimp::Importer importer;
