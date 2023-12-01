f37b84b059702d578bbd8d9e0f89b2bcef31b746
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 14:05:48 -0800

    Store textures in Material

--- ModelLoading/src/Material.cpp
+++ ModelLoading/src/Material.cpp
@@ -1,7 +1,23 @@
 #include "Material.h"
 
+#include <format>
+#include <stdexcept>
+
+Material::Material(ShaderProgram shader, TextureMap textures) :
+	shader(std::move(shader)),
+	textures(std::move(textures))
+{
+	for (const auto& [unit, texture] : this->textures)
+	{
+		if (texture == nullptr)
+		{
+			throw std::runtime_error(std::format("Invalid texture for texture unit {}: must be non-null", unit));
+		}
+	}
+}
+
 Material::Material(ShaderProgram shader) :
-	shader(std::move(shader))
+	Material(std::move(shader), TextureMap())
 {
 }
 
--- ModelLoading/src/Material.h
+++ ModelLoading/src/Material.h
@@ -1,14 +1,22 @@
 #pragma once
 
+#include <map>
+#include <memory>
+
 #include "ShaderProgram.h"
+
+class Texture;
 
 class Material
 {
 public:
+	using TextureMap = std::map<unsigned int, std::shared_ptr<Texture>>;
+	Material(ShaderProgram shader, TextureMap textures);
 	Material(ShaderProgram shader);
 
 	void Bind() const;
 
 private:
 	ShaderProgram shader;
+	TextureMap textures;
 };
