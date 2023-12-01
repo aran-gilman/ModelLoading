fb16704bb2aaac18c6e0a24931d28b989151fada
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 10:32:50 -0800

    Read shader source from files

--- ModelLoading/src/main.cpp
+++ ModelLoading/src/main.cpp
@@ -7,41 +7,12 @@
 
 #include "Camera.h"
 #include "CameraController.h"
+#include "FileUtil.h"
 #include "MeshData.h"
 #include "MeshRenderer.h"
 #include "Shader.h"
 #include "ShaderProgram.h"
 #include "Window.h"
-
-const char* vertexShaderSource = R"s(
-#version 460 core
-layout (location = 0) in vec3 inPos;
-layout (location = 1) in vec3 inNormal;
-
-layout (std140, binding = 0) uniform Camera
-{
-    mat4 view;
-    mat4 projection;
-} camera;
-
-out vec3 Normal;
-
-void main()
-{
-    gl_Position = camera.projection * camera.view * vec4(inPos, 1.0f);
-    Normal = inNormal;
-})s";
-
-const char* fragmentShaderSource = R"s(
-#version 460 core
-out vec4 FragColor;
-
-in vec3 Normal;
-
-void main()
-{
-    FragColor = vec4(abs(Normal), 1.0f);
-})s";
 
 class Scene
 {
@@ -50,8 +21,8 @@ public:
 		camera(glm::vec3(0.0f, 0.0f, 3.0f), 800.0f / 600.0f, 45.0f),
 		cameraController(&camera)
 	{
-		VertexShader vertexShader(vertexShaderSource);
-		FragmentShader fragmentShader(fragmentShaderSource);
+		VertexShader vertexShader(ReadTextFile("resources/shaders/standard.vert"));
+		FragmentShader fragmentShader(ReadTextFile("resources/shaders/normalColor.frag"));
 		std::shared_ptr<ShaderProgram> shaderProgram = std::make_shared<ShaderProgram>(vertexShader, fragmentShader);
 
 		AddModel(LoadModel("resources/models/bookA.fbx"), shaderProgram);
