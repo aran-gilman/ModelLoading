a23d1084d96fec265ff9fa351dcd54264e8f76c4
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 10:31:26 -0800

    Provide more meaningful context when throwing exception

--- ModelLoading/src/FileUtil.cpp
+++ ModelLoading/src/FileUtil.cpp
@@ -1,13 +1,16 @@
 #include "FileUtil.h"
 
+#include <format>
 #include <fstream>
 #include <sstream>
 
 std::string ReadTextFile(const std::string& path)
 {
-    std::ifstream fileStream;
-    fileStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    fileStream.open(path);
+    std::ifstream fileStream(path);
+    if (!fileStream.is_open())
+    {
+        throw std::runtime_error(std::format("Failed to open file: {}", path));
+    }
     std::stringstream stringStream;
     stringStream << fileStream.rdbuf();
     return stringStream.str();
