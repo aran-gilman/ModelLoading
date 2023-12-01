5fcf89b88e24fb7650b5a02ee06d366f57dfde4c
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 13:01:03 -0800

    Add constants for vec2

--- ModelLoading/src/VectorConstants.h
+++ ModelLoading/src/VectorConstants.h
@@ -4,6 +4,17 @@
 
 namespace constants
 {
+	namespace vec2
+	{
+		inline constexpr glm::vec2 Right{ 1.0f, 0.0f };
+		inline constexpr glm::vec2 Left{ -1.0f, 0.0f };
+		inline constexpr glm::vec2 Up{ 0.0f, 1.0f };
+		inline constexpr glm::vec2 Down{ 0.0f, -1.0f };
+
+		inline constexpr glm::vec2 Zero{ 0.0f, 0.0f };
+		inline constexpr glm::vec2 One{ 1.0f, 1.0f };
+	}
+
 	namespace vec3
 	{
 		inline constexpr glm::vec3 Right{ 1.0f, 0.0f, 0.0f };
