14a976bf481cccc83029b8c91d413512a5274170
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 15:35:27 -0800

    Fix loaded image data not being used

--- ModelLoading/src/TextureData.cpp
+++ ModelLoading/src/TextureData.cpp
@@ -8,14 +8,14 @@
 
 TextureData ReadImageFile(const std::string& path)
 {
-	TextureData textureData;
+	TextureData textureData{};
 	int nrChannels;
 	stbi_set_flip_vertically_on_load(true);
-	unsigned char* data = stbi_load(path.c_str(), &textureData.width, &textureData.height, &nrChannels, 0);
-	if (data == nullptr)
+	textureData.data = stbi_load(path.c_str(), &textureData.width, &textureData.height, &nrChannels, 0);
+	if (textureData.data == nullptr)
 	{
 		throw std::runtime_error(std::format("Failed to load image: {}", path));
 	}
-	stbi_image_free(data);
+	textureData.hasAlpha = nrChannels == 4;
 	return textureData;
 }
