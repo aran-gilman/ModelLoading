2800bde28ae3015983cdfb8e560c25b3b9376a17
Aran Gilman <f.zoniventris@gmail.com>
2023-12-01 13:47:18 -0800

    Set texture data in Texture

--- ModelLoading/src/Texture.cpp
+++ ModelLoading/src/Texture.cpp
@@ -7,6 +7,21 @@
 Texture::Texture(const TextureData& textureData)
 {
 	glCreateTextures(GL_TEXTURE_2D, 1, &name);
+	glTextureParameteri(name, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(name, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(name, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTextureParameteri(name, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	unsigned int colorFormat = textureData.hasAlpha ? (unsigned int)GL_RGBA : (unsigned int)GL_RGB;
+	glTextureStorage2D(
+		name, GL_TEXTURE_2D, colorFormat,
+		textureData.width, textureData.height);
+	glTextureSubImage2D(
+		name, GL_TEXTURE_2D,
+		0, 0,
+		textureData.width, textureData.height,
+		colorFormat, GL_UNSIGNED_BYTE,
+		textureData.data);
 }
 
 Texture::~Texture()
